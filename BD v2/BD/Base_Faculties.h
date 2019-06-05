4f1b5740195cfab9d36c7edc7ab84eb7eba65dd2
MIB <48214233+DarthBarada@users.noreply.github.com>
2019-06-05 21:09:20 +0300

    change sort functions
    Пофикшены функции сортировки, которые ничего не меняли

--- BD v2/BD/Base_Faculties.h
+++ BD v2/BD/Base_Faculties.h
@@ -1,26 +1,43 @@
 #pragma once
 #include "Facult.h"
-#include <unordered_map>
+#include <map>
 #include <algorithm>
 
-class base_facult: virtual public facult
+class base_facult: virtual public Facult
 	{
+		std::multimap <std::string, Facult> base_faculties;
 	public:
 // Функция сортировки дисциплин по кафедрам
 		void sort()
 			{
 	/*	Так как multimap - коллекция пар ключ-значение, отсортированная по ключам,то
 			изменяем все значения ключей на кафедры соответствующего контейнера         */
-				if (!get_disciplines()->empty())
+			std::multimap <std::string, Facult>* faculties = new std::multimap <std::string, Facult>;
+			Facult* temp_Facult = new Facult;
+				for (auto i = base_faculties.begin(); i != base_faculties.end(); i++)
 					{
-						std::for_each(get_disciplines()->begin(), get_disciplines()->end(), [](std::pair <std::string, discipline> temp)
+						temp_Facult->get_facult_name() = i->second.get_facult_name();
+						temp_Facult->get_HYK() = i->second.get_HYK();
+						temp_Facult->get_count_of_kaf() = i->second.get_count_of_kaf();
+						temp_Facult->get_unic_disciplines() = i->second.get_unic_disciplines();
+
+						if (!i->second.get_disciplines()->empty())
 							{
-								temp.first = temp.second.get_kafedra();
-							});
+								for (auto j = i->second.get_disciplines()->begin(); j != i->second.get_disciplines()->end(); j++)
+									{
+										temp_Facult->get_disciplines()->insert(std::pair<std::string, discipline>(j->second.get_kafedra(),j->second));
+									}
+							}
+						faculties->insert(std::pair <std::string, Facult>(temp_Facult->get_facult_name(), *temp_Facult));
+						temp_Facult->get_disciplines()->clear();
 					}
-				else
-					{
-						std::cout << "Nothing to sort!\n";
-					}
+				base_faculties.clear(); base_faculties = *faculties;
+				delete temp_Facult;
+				delete faculties;
+			}
+	
+		std::multimap <std::string, Facult>* get_base_faculties()
+			{
+				return &base_faculties;
 			}
 	};
--- BD v2/BD/Branch_Faculties.h
+++ BD v2/BD/Branch_Faculties.h
@@ -3,24 +3,42 @@
 #include <unordered_map>
 #include <algorithm>
 
-class branch_facult: virtual public facult
+class branch_facult: virtual public Facult
 	{
+		std::multimap <std::string, Facult> branch_faculties;
 	public:
 // Функция сортировки дисциплин по организациям
 		void sort()
 			{
-		/*	Так как multimap - коллекция пар ключ-значение, отсортированная по ключам,то 
-			   изменяем все значения ключей на организации соответствующего контейнера      */
-				if (!get_disciplines()->empty())
+	/*	Так как multimap - коллекция пар ключ-значение, отсортированная по ключам,то 
+		   изменяем все значения ключей на организации соответствующего контейнера      */
+				std::multimap <std::string, Facult>* faculties = new std::multimap <std::string, Facult>;
+				Facult* temp_Facult = new Facult;
+				for (auto i = branch_faculties.begin(); i != branch_faculties.end(); i++)
 					{
-						std::for_each(get_disciplines()->begin(), get_disciplines()->end(), [](std::pair <std::string, discipline> temp)
+						temp_Facult->get_facult_name() = i->second.get_facult_name();
+						temp_Facult->get_HYK() = i->second.get_HYK();
+						temp_Facult->get_count_of_kaf() = i->second.get_count_of_kaf();
+						temp_Facult->get_unic_disciplines() = i->second.get_unic_disciplines();
+
+						if (!i->second.get_disciplines()->empty())
 							{
-								temp.first = temp.second.get_organization();
-							});
+								for (auto j = i->second.get_disciplines()->begin(); j != i->second.get_disciplines()->end(); j++)
+									{
+										temp_Facult->get_disciplines()->insert(std::pair<std::string, discipline>(j->second.get_organization(), j->second));
+									}
+							}
+						faculties->insert(std::pair <std::string, Facult>(temp_Facult->get_facult_name(), *temp_Facult));
+						temp_Facult->get_disciplines()->clear();
 					}
-				else
-					{
-						std::cout << "Nothing to sort!\n";
-					}
+				branch_faculties.clear(); branch_faculties = *faculties;
+				delete temp_Facult;
+				delete faculties;
+			}
+
+
+		std::multimap <std::string, Facult>* get_branch_faculties()
+			{
+				return &branch_faculties;
 			}
 	};
